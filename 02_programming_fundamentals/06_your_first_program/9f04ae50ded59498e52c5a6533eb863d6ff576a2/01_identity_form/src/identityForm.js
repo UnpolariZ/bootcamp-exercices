469d3ae64e3640340ff24b4b2c2fc7315aaad227
UnpolariZ <philippe.adisson.pro@gmail.com>
2020-09-09 16:44:28 +0200

    ex name last name age

---
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/01_identity_form/src/identityForm.js
@@ -0,0 +1,13 @@
+const identityForm = (reader) => {
+  reader.question("What's your first Name ? \n> ", (firstName) => {
+    reader.question("What's your last Name ? \n> ", (lastName) => {
+      reader.question("What your age ? \n> ", (age) => {
+        reader.close();
+        console.log ("Your name is $firstName $lastName  and you are $age .");
+      });
+    });
+  });
+};
+// Code here
+// Leave line below for tests to work
+module.exports = identityForm;
