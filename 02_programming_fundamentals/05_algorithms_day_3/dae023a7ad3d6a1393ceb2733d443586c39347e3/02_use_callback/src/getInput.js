ff01f3f587b3794d7fe45fdd65da0fb5fe21e11e
UnpolariZ <philippe.adisson.pro@gmail.com>
2020-09-09 16:44:28 +0200

    exo 2 finish getImput

--- 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
@@ -1,6 +1,9 @@
 // A function that takes two parameters, the second one being a callback function
 
-const getInput = // code the function here
+const getInput = (objet, callback) => {
+  callback(objet.key); 
+};
 
+// code the function here
 // Leave the line below for tests to work
-module.exports = getInput;
+(module.exports = getInput);
