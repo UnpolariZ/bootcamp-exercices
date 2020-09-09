0a9d731698459412e2f1ccdaf7b470eee37f02e2
UnpolariZ <philippe.adisson.pro@gmail.com>
2020-09-09 16:44:28 +0200

    exo 1 finiiiiiiiish yeah

--- 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/01_identity_form/src/identityForm.js
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/01_identity_form/src/identityForm.js
@@ -3,7 +3,7 @@ const identityForm = (reader) => {
     reader.question("What's your last Name ? \n> ", (lastName) => {
       reader.question("What your age ? \n> ", (age) => {
         reader.close();
-        console.log ("Your name is $firstName $lastName  and you are $age .");
+        console.log ( "Your name is "+firstName +" "+ lastName +" and you are "+ age+" .");
       });
     });
   });
--- 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/02_number_game/src/numberGame.js
+++ 02_programming_fundamentals/06_your_first_program/9f04ae50ded59498e52c5a6533eb863d6ff576a2/02_number_game/src/numberGame.js
@@ -1,5 +1,6 @@
-function numberGame(reader, min = 1, max = 100) {
-  // code here
+function numberGame(reader, min = 1, max = 10) {
+  const randomNumber = 
+  reader.close();// code here
 }
 
 module.exports = numberGame;
