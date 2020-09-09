868fbb76bb24ea470225066db0c8d9c1177d4a6c
UnpolariZ <philippe.adisson.pro@gmail.com>
2020-09-09 16:44:28 +0200

    exo 2 / 07 finish

--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/car.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/car.js
@@ -1,5 +1,24 @@
 const car = {
   // Complete here
+  start : function (){
+    this.speed= 0;
+    this.minutes= 0;
+    this.distance = 0;
+    return this;
+  },
+  changeSpeed : function (speed = 130) {
+    this.speed = speed ;
+    return this;
+  },
+  drive : function (minutes = 45) {
+    this.minutes = minutes ;
+    this.distance = this.distance + this.speed * this.minutes / 60 ;
+    return this; 
+  }, 
+  showDistance : function () {
+    console.log(this.distance + " Km");
+    return this;
+  },
 };
 
 module.exports = car;
