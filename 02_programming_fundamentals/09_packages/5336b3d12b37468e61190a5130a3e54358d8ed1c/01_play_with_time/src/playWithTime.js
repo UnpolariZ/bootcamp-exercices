47e0f12dc2ca5c419ac59a35bf3032bdde46cde8
Philippe Adisson <philippe.adisson.pro@gmail.com>
2020-09-21 09:01:42 +0200

    packages

---
+++ 02_programming_fundamentals/09_packages/5336b3d12b37468e61190a5130a3e54358d8ed1c/01_play_with_time/src/playWithTime.js
@@ -0,0 +1,22 @@
+import moment from "moment" ;
+
+export {
+  formatDate,
+  yearsSinceDate,
+  getDayFromDate,
+  daysSinceDate,  
+};
+function formatDate() {
+  moment().format("2000-05-31");
+  return moment ;
+};
+function yearsSinceDate() {
+
+};
+function getDayFromDate() {
+
+};
+
+function daysSinceDate() {
+
+};
