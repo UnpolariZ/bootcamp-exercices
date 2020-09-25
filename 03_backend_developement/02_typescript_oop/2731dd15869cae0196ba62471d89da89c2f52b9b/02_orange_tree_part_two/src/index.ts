3fb6fe7ff055694f4c4e1b70f407d697064bf584
Philippe Adisson <philippe.adisson.pro@gmail.com>
2020-09-25 14:08:46 +0200

    debut orange tree part 2

--- 03_backend_developement/02_typescript_oop/2731dd15869cae0196ba62471d89da89c2f52b9b/02_orange_tree_part_two/src/index.ts
+++ 03_backend_developement/02_typescript_oop/2731dd15869cae0196ba62471d89da89c2f52b9b/02_orange_tree_part_two/src/index.ts
@@ -1,4 +1,33 @@
 // Use this file to test your `OrangeTree` class.
 import { OrangeTree } from "./OrangeTree";
+abstract class Tree {
+    age: number ;
+    height: number = 0;
+    alive: boolean = true;  
+    constructor(age: number){
+        this.age = age;
+        if (age <= 9){
+          this.height = this.height + 25 * age ;
+        }
+        else if (age <= 20) {
+          this.height = this.height + 10 * age + 135;
+        }
+        else if (age > 20){
+            this.height = 335; 
+        }
+    }
+    
+    abstract ageOneYear(): void ;   
+    abstract isAlive(): boolean; 
+    seed():void{
+        this.age = 0; 
+        this.height =0;
+        this.alive = true; 
+    }
+}     
+// Code the class here.
+
+// Leave the line below for tests to work properly.
+export { Tree };
 
 const youngOrangeTree = new OrangeTree(0);
